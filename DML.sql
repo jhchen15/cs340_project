dcaebd4da0dd35d1f4948616000f3651f6de96f3
James Chen <chenjame@oregonstate.edu>
2025-11-03 22:03:22 -0800

    Schools, Players and Games basic CRUD UI set up

---
+++ DML.sql
@@ -0,0 +1,2 @@
+SELECT schoolID AS 'ID', name AS 'Name', address AS 'Address', phone AS 'Phone'
+FROM Schools;
--- app.py
+++ app.py
@@ -27,9 +27,7 @@ def schools():
     try:
         dbConnection = db.connectDB()  # Open our database connection
 
-        # Create and execute our queries
-        # In query1, we use a JOIN clause to display the names of the homeworlds,
-        #       instead of just ID values
+        # Retrieve list of Schools and associated info
         query = ("SELECT schoolID AS 'ID', name AS 'Name', address AS 'Address', phone AS 'Phone' "
                  "FROM Schools;")
         schools = db.query(dbConnection, query).fetchall()
@@ -54,16 +52,14 @@ def players():
     try:
         dbConnection = db.connectDB()  # Open our database connection
 
-        # Create and execute our queries
-        # In query1, we use a JOIN clause to display the names of the homeworlds,
-        #       instead of just ID values
-        query = ("SELECT p.playerID, a.firstName, a.lastName, "
+        # Retrieve list of Players with their Athlete / Team / School info
+        query1 = ("SELECT p.playerID, a.athleteID, t.teamID, a.firstName, a.lastName, "
                  "s.name AS schoolName, t.sportType, t.varsityJv, t.seasonName, "
                  "t.academicYear, a.isEligible, a.isActive "
                  "FROM Players AS p JOIN Athletes AS a ON p.athleteID = a.athleteID "
                  "JOIN Teams AS t ON p.teamID = t.teamID "
                  "JOIN Schools AS s ON s.schoolID = a.schoolID ;")
-        players = db.query(dbConnection, query).fetchall()
+        players = db.query(dbConnection, query1).fetchall()
 
         # Render schools.j2 file, and send school query results
         return render_template(
@@ -79,6 +75,46 @@ def players():
         if "dbConnection" in locals() and dbConnection:
             dbConnection.close()
 
+
+@app.route("/games", methods=["GET"])
+def games():
+    try:
+        dbConnection = db.connectDB()  # Open our database connection
+
+        # Retrieve scheduled games list with associated details
+        query1 = ("SELECT g.gameID, g.homeTeamID, g.awayTeamID, "
+                  "ht.teamName AS homeTeamName, at.teamName AS awayTeamName, "
+                  "g.facilityID, s.name AS facilitySchool, f.facilityName, "
+                  "g.gameDate, g.gameTime, g.gameType, g.status "
+                  "FROM Games AS g JOIN Teams AS ht ON g.homeTeamID = ht.teamID "
+                  "JOIN Teams AS at ON g.awayTeamID = at.teamID "
+                  "JOIN Facilities AS f ON g.facilityID = f.facilityID "
+                  "JOIN Schools AS s ON s.schoolID = f.schoolID ;")
+        games = db.query(dbConnection, query1).fetchall()
+
+        # Retrieve team list with associated details
+        query2 = ("SELECT teamID, schoolID, teamName, sportType, varsityJv, seasonName, academicYear "
+                  "FROM Teams")
+        teams = db.query(dbConnection, query2).fetchall()
+
+        # Retrieve list of facilities
+        query3 = ("SELECT facilityID, schoolID, facilityName, capacity "
+                  "FROM Facilities")
+        facilities = db.query(dbConnection, query3).fetchall()
+
+        # Render games.j2 file, and send game query results
+        return render_template(
+            "games.j2", games=games, teams=teams, facilities=facilities
+        )
+
+    except Exception as e:
+        print(f"Error executing queries: {e}")
+        return "An error occurred while executing the database queries.", 500
+
+    finally:
+        # Close the DB connection, if it exists
+        if "dbConnection" in locals() and dbConnection:
+            dbConnection.close()
 
 # ########################################
 # ########## LISTENER
